9469ea7d011bdc5dd54ff4f40a8ef281a462829f
samir ardjouni <samir.ardjouni@gmail.com>
2021-02-17 15:34:37 +0100

    create structure

---
+++ src/test/java/cda/junit/test/TestFormation.java
@@ -0,0 +1,14 @@
+package cda.junit.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFormation {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
