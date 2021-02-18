3e24c3185275f3033e351582a874abe92bd3260c
valentin-cochin <cochin.valentin@hotmail.fr>
2021-02-18 11:30:58 +0100

    Add connexion to AWS instance

--- docs/script_dll.sql
+++ docs/script_dll.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS  cda_junit_jdbc;
-CREATE USER IF NOT EXISTS 'userJunit1'@'localhost' IDENTIFIED BY 'user1pwd';
-GRANT ALL PRIVILEGES ON cda_junit_jdbc . * TO 'userJunit1'@'localhost';
+CREATE USER IF NOT EXISTS 'userJunit1'@'%' IDENTIFIED BY 'user1pwd';
+GRANT ALL PRIVILEGES ON cda_junit_jdbc . * TO 'userJunit1'@'%';
 FLUSH PRIVILEGES;
